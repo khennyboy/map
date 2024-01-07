c857fd3e90099572178ef3a6ab6add36ef3b701e
Khennyboy <abdullateefkehinde848@gmail.com>
2024-01-07 12:10:35 +0100

    refactored

--- js/country.js
+++ js/country.js
@@ -1,20 +1,21 @@
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.container');
-
+let value
+let submit = document.querySelector('.submit_btn');
 function getCountry(name) {
     document.querySelector('.error-message').innerHTML=''
-    countriesContainer.innerHTML='';
+    document.querySelector('.country') && document.body.removeChild(document.querySelector('.country'))
   const request = new XMLHttpRequest();
   request.open('GET', `https://restcountries.com/v3.1/name/${name}`);
   request.send();
 
   request.addEventListener('load', function () {
+    submit.innerHTML='Fetch details';
     if (request.status !== 200) {
-      displayErrorMessage('Country not found the country information you looking for, Please enter a valid country.');
+      displayErrorMessage('Country information not found, Please enter a valid country.');
       return;
     }
     const [data] = JSON.parse(this.responseText);
-    console.log(data)
+    let x = data.name.common.toLowerCase();
+    if (value === x ){
     var html = ` <article class="country">
     <img class="country__img" src="${data.flags.png}" />
     <div class="country__data">
@@ -25,19 +26,28 @@ function getCountry(name) {
       <p class="country__row"><span>💰</span>${Object.values(data.currencies)[0].name} ${Object.values(data.currencies)[0].symbol}</p>
     </div>
   </article>`;
-    countriesContainer.insertAdjacentHTML('afterbegin', html);
+    document.body.insertAdjacentHTML('beforeend', html);
+    }
+    else{
+        displayErrorMessage('Country information not found, Please enter a valid country.');
+    }
   });
 
+
   request.addEventListener('error', function () {
+    submit.innerHTML='Fetch details';
     displayErrorMessage('An error occurred while fetching data.');
   });
 }
-
+// error message function
 function displayErrorMessage(message) {
     document.querySelector('.error-message').innerHTML= message
 }
+// handle submit button
 document.forms['countryForm'].addEventListener('submit', function(e){
+    submit.innerHTML = 'Loading...'
     e.preventDefault()
-    let value = document.forms['countryForm']['country'].value
+    value = document.forms['countryForm']['country'].value.trim()
+    value = value.toLowerCase() 
     getCountry(value)
 })
